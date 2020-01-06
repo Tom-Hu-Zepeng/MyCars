ffa329a49623206e184da0ead107cf033584e67a
Tom <soul960918@163.com>
2020-01-06 15:34:27 +0800

    登录修改

--- src/test/java/com/accp/MyCarsApplicationTests.java
+++ src/test/java/com/accp/MyCarsApplicationTests.java
@@ -1,14 +1,20 @@
 package com.accp;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.accp.biz.h.LoginBiz;
 
 @SpringBootTest //Classes = {MyCarsApplication.class}
 class MyCarsApplicationTests {
+	
+	@Autowired
+	private LoginBiz biz;
 
 	@Test
 	void contextLoads() {
-		System.out.println("出");
+		System.out.println(biz.queryByUsers("Tom", "1111111"));
 	}
 
 }
